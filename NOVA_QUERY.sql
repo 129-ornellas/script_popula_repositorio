c802beab04c5823192571e95e784767b6db0eddd
Marcos Ornellas <marcos.ornellas@amaiseducacao.com.br>
2024-04-18 16:17:05 -0300

    aumenta len do group_concat, ajusta tratamentos de campos string/data e pega opcao tipo

--- NOVA_QUERY.sql
+++ NOVA_QUERY.sql
@@ -1,11 +1,38 @@
+SET SESSION group_concat_max_len = 5000;
+
 SELECT
 	agrup.ds_agrupamento,
-	rel.nm_arquivo,
+
 	rel.nm_relatorio,-- 	arquivos da rgo_relatorios à serem criados :
 	rel.me_relatorio_config,
 	rel.me_relatorio_view,
 	rel.me_relatorio_index,
 	rel.ds_filtro,-- 	inser do relatorio sem os campos acima:
+	rel.cd_relatorio,
+	(
+		SELECT 
+		 GROUP_CONCAT( distinct "INSERT INTO rgo_opcoes_tipos (
+								cd_opcao_tipo,
+								ds_opcao_tipo,
+								ds_tag_opcao,
+								dt_base
+						)
+				VALUES (
+								NULL,",
+								IF(opct.ds_opcao_tipo IS NOT NULL, CONCAT('"',opct.ds_opcao_tipo, '"' ), 'NULL'),
+								',',
+								IF(opct.ds_tag_opcao IS NOT NULL,  CONCAT('"',opct.ds_tag_opcao, '"'), 'NULL'),
+								',',
+								IF(opct.dt_base IS NOT NULL, CONCAT('"', opct.dt_base, '"'), 'NULL'), ')'
+						SEPARATOR '; \n')
+		FROM rgo_opcoes opc
+		INNER JOIN rgo_opcoes_tipos opct ON (opc.cd_opcao_tipo = opct.cd_opcao_tipo)
+		WHERE opc.cd_relatorio = rel.cd_relatorio
+
+	) as opcao_tipo_impressao,
+	
+	
+	
 	CONCAT(
 		"INSERT IGNORE INTO rgo_relatorios (
 		cd_relatorio,
@@ -37,7 +64,7 @@ SELECT
 		',',
 		IFNULL( rel.sn_ativo, ' NULL' ),
 		',',
-		IFNULL( rel.nm_relatorio, ' NULL' ),
+		IF( rel.nm_relatorio IS NOT NULL, CONCAT( '"', rel.nm_relatorio , '"'), ' NULL' ),
 		',',
 		IFNULL( rel.sn_padrao, ' NULL' ),
 		',',
@@ -52,56 +79,27 @@ SELECT
 		',',
 		IFNULL( rel.cd_formato, ' NULL' ),
 		',',
-		IFNULL( rel.me_documentacao_padrao, ' NULL' ),
+		IF( rel.me_documentacao_padrao IS NOT NULL, CONCAT('"' , rel.me_documentacao_padrao ,'"' ) ,' NULL' ),
 		',',
-		IFNULL( rel.me_documentacao_cliente, ' NULL' ),
+		IF( rel.me_documentacao_cliente IS NOT NULL, CONCAT('"', rel.me_documentacao_cliente, '"'), ' NULL' ),
 		',',
-		IFNULL( rel.ds_chave, ' NULL' ),
+		IF( rel.ds_chave IS NOT NULL, CONCAT('"', rel.ds_chave, '"'), ' NULL' ),
 		',',
-		IFNULL( rel.dt_base, ' NULL' ),
+		IF( rel.dt_base IS NOT NULL, CONCAT('"', rel.dt_base, '"') ,' NULL' ),
 		',',
 		IFNULL( rel.sn_autentique, ' NULL' ),
 		',',
-		IFNULL( rel.ds_autentique_config, ' NULL' ) -- 				FALTA TRATAR CASO UM DOS CAMPOS FOR NULO
+		IF( rel.ds_autentique_config IS NOT NULL, CONCAT('"', rel.ds_autentique_config, '"') , ' NULL' ), ')' -- 				FALTA TRATAR CASO UM DOS CAMPOS FOR NULO
 		
+
 	) AS insert_statement,
 	(
-	SELECT
-		GROUP_CONCAT(
-			"INSERT IGNORE INTO rgo_opcoes (
-			cd_opcao,
-			cd_relatorio,
-			cd_opcao_tipo,
-			sn_padrao,
-			sn_ativo,
-			ds_opcao,
-			me_configuracao,
-			ds_chave,
-			dt_base
-			)
-			VALUES (
-			NULL,
-			NULL,
-			NULL,",
-			IFNULL( opc.sn_padrao, 'NULL' ),
-			',',
-			IFNULL( opc.sn_ativo, 'NULL' ),
-			',',
-			IFNULL( opc.ds_opcao, 'NULL' ),
-			',',
-			IFNULL( opc.me_configuracao, 'NULL' ),
-			',',
-			IFNULL( opc.ds_chave, 'NULL' ),
-			',',
-			IFNULL( opc.dt_base, 'NULL' ),
-			'\n )' SEPARATOR '; \n' 
-	)
-	FROM
-		rgo_opcoes opc 
-	WHERE
-		opc.cd_relatorio = rel.cd_relatorio 
-	) AS opcoes_impressao,
 	
+	SELECT GROUP_CONCAT(
+"INSERT IGNORE INTO rgo_opcoes (cd_opcao, cd_relatorio,cd_opcao_tipo,sn_padrao,sn_ativo,ds_opcao,me_configuracao,ds_chave,dt_base)
+VALUES (NULL,NULL,NULL,",IFNULL( opc.sn_padrao, 'NULL' ),',',IFNULL( opc.sn_ativo, 'NULL' ),',',IF( opc.ds_opcao IS NOT NULL, CONCAT('"', opc.ds_opcao, '"'), 'NULL' ),',',IF( opc.me_configuracao IS NOT NULL, CONCAT('"', opc.me_configuracao, '"') ,'NULL' ),',',IF( opc.ds_chave IS NOT NULL, CONCAT('"', opc.ds_chave ,'"') ,'NULL' ),',',IF( opc.dt_base IS NOT NULL, CONCAT('"' , dt_base , '"'), 'NULL' ),'\n )' SEPARATOR '; \n' 	)	
+FROM rgo_opcoes opc 	WHERE		opc.cd_relatorio = rel.cd_relatorio 	) AS opcoes_impressao,
+
 	IF( rel.cd_tipo = 2, 
 		(SELECT opc.me_configuracao FROM rgo_opcoes opc WHERE rel.cd_relatorio = opc.cd_relatorio ), NULL
 	) AS me_config
@@ -109,8 +107,9 @@ SELECT
 FROM
 	rgo_relatorios rel
 	INNER JOIN rgo_agrupamentos agrup ON ( agrup.cd_agrupamento = rel.cd_agrupamento ) 
+	
 WHERE
 	me_relatorio_index IS NOT NULL 
 	AND me_relatorio_index != '' 
 	AND sn_ativo = 1 
-	LIMIT 10
+	AND (rel.nm_arquivo IS NULL OR rel.nm_arquivo != '')
