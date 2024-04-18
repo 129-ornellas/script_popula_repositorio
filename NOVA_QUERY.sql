10e8fc8ae350ce2819d90231e4027b38480bfcc9
Marcos Ornellas <marcos.ornellas@amaiseducacao.com.br>
2024-04-18 13:48:32 -0300

    rgo_opcoes e me_config das listas

--- NOVA_QUERY.sql
+++ NOVA_QUERY.sql
@@ -8,51 +8,104 @@ SELECT
 	rel.ds_filtro,-- 	inser do relatorio sem os campos acima:
 	CONCAT(
 		"INSERT IGNORE INTO rgo_relatorios (
-        cd_relatorio,
-        cd_tipo,
-        cd_agrupamento,
-        sn_ativo,
-        nm_relatorio,
-        sn_padrao,
-        ds_filtro,
-        nm_arquivo,
-        me_relatorio_index,
-        me_relatorio_config,
-        me_relatorio_view,
-        cd_relatorio_template,
-        cd_formato,
-        me_documentacao_padrao,
-        me_documentacao_cliente,
-        ds_chave,
-        dt_base,
-        sn_autentique,
-        ds_autentique_config
+		cd_relatorio,
+		cd_tipo,
+		cd_agrupamento,
+		sn_ativo,
+		nm_relatorio,
+		sn_padrao,
+		ds_filtro,
+		nm_arquivo,
+		me_relatorio_index,
+		me_relatorio_config,
+		me_relatorio_view,
+		cd_relatorio_template,
+		cd_formato,
+		me_documentacao_padrao,
+		me_documentacao_cliente,
+		ds_chave,
+		dt_base,
+		sn_autentique,
+		ds_autentique_config
     )
-VALUES (
-				NULL,
-        ",IFNULL(rel.cd_tipo, ' NULL') ,','
-        ,IFNULL(rel.cd_agrupamento, ' NULL'),','
-        ,IFNULL(rel.sn_ativo, ' NULL'),','
-        ,IFNULL(rel.nm_relatorio, ' NULL'),','
-        ,IFNULL(rel.sn_padrao, ' NULL'),',',"
-				 NULL,
-				 NULL,
-				 NULL,
-				 NULL,
-				 NULL,
-				"
-				, IFNULL(rel.cd_relatorio_template, ' NULL'),','
-        , IFNULL(rel.cd_formato, ' NULL'), ','
-        , IFNULL(rel.me_documentacao_padrao, ' NULL'), ','
-        , IFNULL(rel.me_documentacao_cliente, ' NULL'),','
-        , IFNULL(rel.ds_chave, ' NULL'),','
-        , IFNULL(rel.dt_base, ' NULL'), ','
-        , IFNULL(rel.sn_autentique, ' NULL'),','
-        , IFNULL(rel.ds_autentique_config, ' NULL')
-				
--- 				FALTA TRATAR CASO UM DOS CAMPOS FOR NULO
-        
-	) AS insert_statement 
+		VALUES (
+		NULL,
+		",
+		IFNULL( rel.cd_tipo, ' NULL' ),
+		',',
+		IFNULL( rel.cd_agrupamento, ' NULL' ),
+		',',
+		IFNULL( rel.sn_ativo, ' NULL' ),
+		',',
+		IFNULL( rel.nm_relatorio, ' NULL' ),
+		',',
+		IFNULL( rel.sn_padrao, ' NULL' ),
+		',',
+		"
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		",
+		IFNULL( rel.cd_relatorio_template, ' NULL' ),
+		',',
+		IFNULL( rel.cd_formato, ' NULL' ),
+		',',
+		IFNULL( rel.me_documentacao_padrao, ' NULL' ),
+		',',
+		IFNULL( rel.me_documentacao_cliente, ' NULL' ),
+		',',
+		IFNULL( rel.ds_chave, ' NULL' ),
+		',',
+		IFNULL( rel.dt_base, ' NULL' ),
+		',',
+		IFNULL( rel.sn_autentique, ' NULL' ),
+		',',
+		IFNULL( rel.ds_autentique_config, ' NULL' ) -- 				FALTA TRATAR CASO UM DOS CAMPOS FOR NULO
+		
+	) AS insert_statement,
+	(
+	SELECT
+		GROUP_CONCAT(
+			"INSERT IGNORE INTO rgo_opcoes (
+			cd_opcao,
+			cd_relatorio,
+			cd_opcao_tipo,
+			sn_padrao,
+			sn_ativo,
+			ds_opcao,
+			me_configuracao,
+			ds_chave,
+			dt_base
+			)
+			VALUES (
+			NULL,
+			NULL,
+			NULL,",
+			IFNULL( opc.sn_padrao, 'NULL' ),
+			',',
+			IFNULL( opc.sn_ativo, 'NULL' ),
+			',',
+			IFNULL( opc.ds_opcao, 'NULL' ),
+			',',
+			IFNULL( opc.me_configuracao, 'NULL' ),
+			',',
+			IFNULL( opc.ds_chave, 'NULL' ),
+			',',
+			IFNULL( opc.dt_base, 'NULL' ),
+			'\n )' SEPARATOR '; \n' 
+	)
+	FROM
+		rgo_opcoes opc 
+	WHERE
+		opc.cd_relatorio = rel.cd_relatorio 
+	) AS opcoes_impressao,
+	
+	IF( rel.cd_tipo = 2, 
+		(SELECT opc.me_configuracao FROM rgo_opcoes opc WHERE rel.cd_relatorio = opc.cd_relatorio ), NULL
+	) AS me_config
+
 FROM
 	rgo_relatorios rel
 	INNER JOIN rgo_agrupamentos agrup ON ( agrup.cd_agrupamento = rel.cd_agrupamento ) 
@@ -60,4 +113,4 @@ WHERE
 	me_relatorio_index IS NOT NULL 
 	AND me_relatorio_index != '' 
 	AND sn_ativo = 1 
-	LIMIT 2
+	LIMIT 10
